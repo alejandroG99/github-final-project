83c575c4869ae713229dcfbf4fb57ca705687bbe
alejandroG99 <72236512+alejandroG99@users.noreply.github.com>
2023-06-09 10:06:45 -0600

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <alejandroG99>
    # Input:
    # p, principal amount
    # t, time period in years
